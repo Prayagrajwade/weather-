7edee7fb6a7553b7888020e5c982eb3bbbd9f3e2
prayag <prayagrajwade01@gmail.com>
2021-07-21 17:57:11 +0530

    get data from api complete

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ let weather = {
     apiKey:"c0bad55d6ea4b148ca54752e1d0933d3",
     fetchWeather: function (city)
     {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey).then((response) => response.json()).then((data) => console.log(data));
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey).then((response) => response.json()).then((data) => this.displayWeather(data));
     },
 
 
     displayWeather: function(data){
         const{name} = data;
+        const {icon,discription} = data.weather;
+        const{temp,humidity} = data.main;
+        const{speed} = data.wind;
+        console.log(name,icon,discription,temp,humidity,speed)
     }
 };
