7688e9ad83b8facdd753a1605f4828260b86eb76
prayag <prayagrajwade01@gmail.com>
2021-07-21 17:46:54 +0530

    frist commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let weather = {
+    apiKey:"c0bad55d6ea4b148ca54752e1d0933d3",
+    fetchWeather: function (city)
+    {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey).then((response) => response.json()).then((data) => console.log(data));
+    },
+
+
+    displayWeather: function(data){
+
+    }
+};
